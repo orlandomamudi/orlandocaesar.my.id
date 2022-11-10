1c0b58696606b4f61292078315bfe89b1950adf1
orlandomamudi <orlando.c.mamudi@gmail.com>
2022-11-10 16:52:50 +0800

    First Init

---
+++ assets/custom.js
@@ -0,0 +1,12 @@
+// SIDENAV
+$(document).ready(function () {
+    $('.sidenav').sidenav();
+
+    $('.dark-toggle').on('click', function () {
+        if ($(this).find('i').text() == 'brightness_4') {
+            $(this).find('i').text('brightness_high');
+        } else {
+            $(this).find('i').text('brightness_4');
+        }
+    });
+});
