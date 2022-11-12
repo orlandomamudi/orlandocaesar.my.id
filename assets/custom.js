6bbba30fc332618c4ee733156f457b6cae8cb43b
orlandomamudi <orlando.c.mamudi@gmail.com>
2022-11-12 09:03:08 +0800

    First Init

--- assets/custom.js
+++ assets/custom.js
@@ -1,5 +1,6 @@
 // SIDENAV
 $(document).ready(function () {
+    localStorage.getItem('mode') === 'dark' ? document.querySelector('body').classList.add('dark') : document.querySelector('body').classList.remove('dark');
     $('.sidenav').sidenav();
 
     $('.dark-toggle').on('click', function () {
