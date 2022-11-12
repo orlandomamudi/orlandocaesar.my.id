86fccbc70ed9cbadf7a61adf66acd2c99d5e095f
orlandomamudi <orlando.c.mamudi@gmail.com>
2022-11-12 13:39:43 +0800

    update

--- assets/custom.js
+++ assets/custom.js
@@ -3,10 +3,16 @@ $(document).ready(function () {
     localStorage.getItem('mode') === 'dark' ? document.querySelector('body').classList.add('dark') : document.querySelector('body').classList.remove('dark');
     $('.sidenav').sidenav();
 
+    if (localStorage.getItem('mode') === 'dark') {
+        $(this).find('i').text('brightness_high');
+    } else if (localStorage.getItem('mode') === 'light') {
+        $(this).find('i').text('brightness_4');
+    }
+
     $('.dark-toggle').on('click', function () {
-        if ($(this).find('i').text() == 'brightness_4') {
+        if (localStorage.getItem('mode') === 'dark') {
             $(this).find('i').text('brightness_high');
-        } else {
+        } else if (localStorage.getItem('mode') === 'light') {
             $(this).find('i').text('brightness_4');
         }
     });
